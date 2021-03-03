38c772c21927b9bf91f0a2382d05458107465ed9
Jack <jackstephens93@hotmail.co.uk>
2021-03-03 17:32:34 +0000

    taking shape..

---
+++ 1_mlhc.py
@@ -0,0 +1,18 @@
+from utils.etl.fetch_data import load_housing_data
+
+
+# 1) Get data
+housing = load_housing_data()
+
+# 2) Explore the data
+housing.hist(bins=50, figsize=(20, 15))
+
+# 3) Prepare the data
+
+# 4) Shortlist promising models
+
+# 5) Fine-tune the system
+
+# 6) Present solution
+
+# 7) Launch model
