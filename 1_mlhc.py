eb8b0e42f0fea3db8e0007902bf2983bfb5f1398
Jack <jackstephens93@hotmail.co.uk>
2021-03-03 20:59:46 +0000

    next step - data prep

--- 1_mlhc.py
+++ 1_mlhc.py
@@ -1,11 +1,38 @@
 from utils.etl.fetch_data import load_housing_data
 
+from sklearn.model_selection import train_test_split
+
+import matplotlib.pyplot as plt
 
 # 1) Get data
-housing = load_housing_data()
+housing_data = load_housing_data()
+
+train_set, test_set = train_test_split(housing_data, test_size=0.2, random_state=42)
+
+df = train_set
 
 # 2) Explore the data
-housing.hist(bins=50, figsize=(20, 15))
+# Histogram
+df.hist(bins=50, figsize=(20, 15))
+
+# Geographic plot
+df.plot(
+    kind='scatter',
+    x='longitude',
+    y='latitude',
+    alpha=0.4,
+    s=df['population']/100,
+    label="Population",
+    figsize=(10, 7),
+    c="median_house_value",
+    cmap=plt.get_cmap("jet"),
+    colorbar=True,
+    sharex=False
+)
+
+# Correlation of features to y variable
+corr_matrix = df.corr()
+corr_matrix["median_house_value"].sort_values(ascending=False)
 
 # 3) Prepare the data
 
