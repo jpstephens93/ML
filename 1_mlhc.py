c4c7cd26abdeb1bfd93e2ff605861be8c1fdee87
Jack <jackstephens93@hotmail.co.uk>
2021-03-10 18:43:18 +0000

    updates

--- 1_mlhc.py
+++ 1_mlhc.py
@@ -1,27 +1,32 @@
 from utils.etl.fetch_data import load_housing_data
+from utils.etl.sklearn_extender import CombinedAttributesAdder
 
 from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
 
+import pandas as pd
 import matplotlib.pyplot as plt
 
 # 1) Get data
 housing_data = load_housing_data()
 
-train_set, test_set = train_test_split(housing_data, test_size=0.2, random_state=42)
-
-df = train_set
-
 # 2) Explore the data
+print(housing_data.info())
+print(housing_data.describe())
+
 # Histogram
-df.hist(bins=50, figsize=(20, 15))
+housing_data.hist(bins=50, figsize=(20, 15))
 
 # Geographic plot
-df.plot(
+housing_data.plot(
     kind='scatter',
     x='longitude',
     y='latitude',
     alpha=0.4,
-    s=df['population']/100,
+    s=housing_data['population']/100,
     label="Population",
     figsize=(10, 7),
     c="median_house_value",
@@ -31,10 +36,26 @@ df.plot(
 )
 
 # Correlation of features to y variable
-corr_matrix = df.corr()
-corr_matrix["median_house_value"].sort_values(ascending=False)
+corr_matrix = housing_data.corr()
+print(corr_matrix["median_house_value"].sort_values(ascending=False))
 
 # 3) Prepare the data
+# a) Data Cleaning
+train_set, test_set = train_test_split(housing_data, test_size=0.2, random_state=42)
+
+df = train_set.drop(columns=['median_house_value'])
+df_numerical = df.drop(columns=['ocean_proximity'])
+
+df_numerical_pipeline = Pipeline([
+    ('imputer', SimpleImputer(strategy='median')),
+    ('attribs_adder', CombinedAttributesAdder()),
+    ('std_scaler', StandardScaler())
+])
+
+num_attribs = list(df_numerical)
+cat_attribs = ["ocean_proximity"]
+
+full_pipeline = ColumnTransformer([("num", df_numerical_pipeline, num_attribs), ("cat", OneHotEncoder(), cat_attribs)])
 
 # 4) Shortlist promising models
 
